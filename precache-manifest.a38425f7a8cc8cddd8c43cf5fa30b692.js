203faab8bc0db6c20795e9ccd95b01d342fd5d1d
whoque16@gmail.com <whoque16@gmail.com>
2020-04-06 13:34:22 +0530

    Updates

---
+++ precache-manifest.a38425f7a8cc8cddd8c43cf5fa30b692.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "090dab87dc2aed08ff9b5e4f329a91a6",
+    "url": "/mini-piano/index.html"
+  },
+  {
+    "revision": "00c3131dd1982064756d",
+    "url": "/mini-piano/static/css/main.76de4e10.chunk.css"
+  },
+  {
+    "revision": "95c2449ecd561b85845e",
+    "url": "/mini-piano/static/js/2.4042a544.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mini-piano/static/js/2.4042a544.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00c3131dd1982064756d",
+    "url": "/mini-piano/static/js/main.70e51674.chunk.js"
+  },
+  {
+    "revision": "895589be8bf4f22ad2eb",
+    "url": "/mini-piano/static/js/runtime-main.48a60966.js"
+  }
+]);
